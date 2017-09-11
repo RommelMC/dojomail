2691165164f9c5e80875ffea5107f2f9e76c7497
Rommel Castro <Rommel@Rommels-MacBook-Pro-2.local>
2017-09-11 14:19:41 -0700

    first commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  emails=[
+    {email: 'bill@gates.com', importance: true, subject:'new windows', content:'windows xi will launch in 2100'},
+    {email: 'ada@lovelace.com', importance: true, subject:'programming', content:'enchantress of numbers'},
+    {email: 'john@carmac.com', importance: false, subject:'updated algo', content:'new algorithm for shadow volumes'},
+    {email: 'gabe@newel.com', importance: false, subject:'hl3!', content:'just kidding'}
+  ]
+}
